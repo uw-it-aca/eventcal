00c47fdabfe51a7109aa4037ca63e313423edbf6
Fang Lin <fanglin@uw.edu>
2019-07-15 16:19:30 -0700

    fix INSTALLED_APPS

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -16,7 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accountsynchr.app.EventCalConfig',
+    'accountsynchr.apps.EventCalConfig'
 ]
 
 MIDDLEWARE_CLASSES = [
