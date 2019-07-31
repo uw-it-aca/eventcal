4a943cad91bbfc230c42f1bd4f08cf05b42b46a8
Fang Lin <fanglin@uw.edu>
2019-07-30 21:05:00 -0700

    up uw_gws version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         'Django>=2.0.13,<2.1',
         'UW-RestClients-Core>1.2,<2.0',
-        'UW-RestClients-GWS>=2.2,<3.0',
+        'UW-RestClients-GWS>=2.2.1,<3.0',
         'UW-RestClients-Trumba>=1.2.5,<2.0',
         'pyopenssl'
         ],
