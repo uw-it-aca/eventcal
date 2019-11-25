33ede5063d9b0a5b401009c6f413c7076d89c808
Fang Lin <fanglin@uw.edu>
2019-11-25 15:57:03 -0800

    update docs

--- accountsynchr/dao/notifier.py
+++ accountsynchr/dao/notifier.py
@@ -1,11 +1,3 @@
-"""
-Send Trumba account removal email
-Required settings:
-    EMAIL_BACKEND
-    EMAIL_HOST
-    EMAIL_PORT
-"""
-
 import logging
 from django.core.mail import send_mail
 from accountsynchr.util.settings import (
@@ -16,6 +8,18 @@ logger = logging.getLogger(__name__)
 
 
 def send_acc_removal_email(uwnetid):
+    """
+    Send Trumba account removal email
+    Required settings:
+      EMAIL_BACKEND
+      EMAIL_HOST
+      EMAIL_PORT
+      EMAIL_ADDRESS_DOMAIN
+      EMAIL_SENDER
+      PURGE_EMAIL_MESSAGE
+      PURGE_EMAIL_SUBJECT
+    """
+
     email_address_domain = get_email_address_domain()
     recipient = "{}{}".format(uwnetid, email_address_domain)
     try:
