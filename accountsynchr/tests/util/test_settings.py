1a36c9580f16b80906dcdeb2045fe414edf685eb
Fang Lin <fanglin@uw.edu>
2020-07-27 13:18:20 -0700

    fix style

--- accountsynchr/tests/util/test_settings.py
+++ accountsynchr/tests/util/test_settings.py
@@ -13,24 +13,20 @@ class TestSettings(TestCase):
 
     def test_get_email_address_domain(self):
         with self.settings(EMAIL_ADDRESS_DOMAIN='@test.edu'):
-            self.assertEqual(get_email_address_domain(),
-                             "@test.edu")
+            self.assertEqual(get_email_address_domain(), "@test.edu")
 
     def test_get_email_sender(self):
         with self.settings(EMAIL_SENDER='eventcal@test.edu'):
-            self.assertEqual(get_email_sender(),
-                             "eventcal@test.edu")
+            self.assertEqual(get_email_sender(), "eventcal@test.edu")
 
     def test_get_email_message(self):
         with self.settings(PURGE_EMAIL_MESSAGE='Body'):
-            self.assertEqual(get_email_message(),
-                             "Body")
+            self.assertEqual(get_email_message(), "Body")
 
     def test_get_email_subject(self):
         with self.settings(PURGE_EMAIL_SUBJECT='Subject'):
-            self.assertEqual(get_email_subject(),
-                             "Subject")
+            self.assertEqual(get_email_subject(), "Subject")
 
     def test_get_cronjob_sender(self):
         with self.settings(CRONJOB_SENDER='none'):
-             self.assertEqual(get_cronjob_sender(), 'none')
+            self.assertEqual(get_cronjob_sender(), 'none')
