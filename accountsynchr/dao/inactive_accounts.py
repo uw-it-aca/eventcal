8a71f8f3b996040298ca5c59dcd0c0d663190d9b
Fang Lin <fanglin@uw.edu>
2021-01-04 13:32:26 -0800

    Enable another csv format for the account file

--- accountsynchr/dao/inactive_accounts.py
+++ accountsynchr/dao/inactive_accounts.py
@@ -12,11 +12,11 @@ from accountsynchr.dao.notifier import send_acc_removal_email
 logger = logging.getLogger(__name__)
 
 
-def get_file_path():
+def get_file_path(filename='accounts.csv'):
     file_path = get_csv_file_path()
     if file_path is None:
         file_path = os.path.join(os.path.dirname(__file__), '..', 'data')
-    return os.path.join(file_path, 'accounts.csv')
+    return os.path.join(file_path, filename)
 
 
 def get_accounts_to_purge(existing_group_member_set,
@@ -78,3 +78,53 @@ def get_accounts_to_purge(existing_group_member_set,
 
 def str_to_datetime(s):
     return parse(s) if (s is not None and len(s)) else None
+
+
+def get_accounts_to_purge1(existing_group_member_set,
+                           notify_inactive_users=False):
+    """
+    Use Steve exported account file.
+    Also email users to be purged in the next month.
+    returns: 1. a list of UserAccounts of the users to be purged
+             2. a set of uwnetids of the users to be purged
+    """
+    user_records = []  # store in a list
+    user_set = set()  # store in a set
+    total_notified_users = 0
+    total_notify_err = 0
+
+    email_address_domain = get_email_address_domain()
+    path = get_file_path(filename='accounts1.csv')
+    reader = csv.reader(open(path, 'r', encoding='utf8'), delimiter=',')
+    next(reader)
+    for line in reader:
+        try:
+            if line[1].endswith(email_address_domain):
+                created_at = str_to_datetime(line[4])
+                last_visit = str_to_datetime(line[5])
+                acc = UserAccount(
+                    uwnetid=re.sub(
+                        email_address_domain, "", line[1], flags=re.I),
+                    created_at=created_at, last_visit=last_visit)
+
+                if int(line[7]) > 420:
+                    # Will be purged in this run
+                    user_records.append(acc)
+                    user_set.add(acc.uwnetid)
+                elif int(line[7]) > 360:
+                    # Notify user before purging
+                    if notify_inactive_users:
+                        if send_acc_removal_email(acc.uwnetid):
+                            total_notified_users += 1
+                        else:
+                            total_notify_err += 1
+                else:
+                    continue
+        except Exception as ex:
+            logger.error("{} in line: {}".format(ex, line))
+    if notify_inactive_users:
+        logger.info("Notified {} users".format(total_notified_users))
+        logger.info("{} errors when sending notification email".format(
+                total_notify_err))
+
+    return user_records, user_set
--- accountsynchr/tests/dao/test_inactive_accounts.py
+++ accountsynchr/tests/dao/test_inactive_accounts.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.conf import settings
 from accountsynchr.dao.inactive_accounts import (
-    get_accounts_to_purge, get_file_path)
+    get_accounts_to_purge1, get_accounts_to_purge, get_file_path)
 
 
 class TestInactiveAccounts(TestCase):
@@ -14,6 +14,16 @@ class TestInactiveAccounts(TestCase):
         with self.settings(CSV_FILE_PATH=None,
                            EMAIL_ADDRESS_DOMAIN='@test.edu'):
             accounts_to_purge, user_set = get_accounts_to_purge(set())
+            self.assertEqual(len(accounts_to_purge), 2)
+            self.assertTrue('sdummys' in user_set)
+            self.assertTrue('sdummyp' in user_set)
+            self.assertEqual(accounts_to_purge[0].uwnetid, 'sdummys')
+            self.assertEqual(accounts_to_purge[1].uwnetid, 'sdummyp')
+
+    def test_get_accounts_to_purge1(self):
+        with self.settings(CSV_FILE_PATH=None,
+                           EMAIL_ADDRESS_DOMAIN='@test.edu'):
+            accounts_to_purge, user_set = get_accounts_to_purge1(set())
             self.assertEqual(len(accounts_to_purge), 2)
             self.assertTrue('sdummys' in user_set)
             self.assertTrue('sdummyp' in user_set)
