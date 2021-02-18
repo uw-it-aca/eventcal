325f481afbce8d053f040581d43b71545986c612
Fang Lin <fanglin@uw.edu>
2021-02-18 15:06:12 -0800

    Develop (#92)
    enable deployment on develop branch

--- accountsynchr/management/commands/external_group.py
+++ accountsynchr/management/commands/external_group.py
@@ -20,10 +20,9 @@ class Command(BaseCommand):
                             TrumbaCalendar.TAC_CAMPUS_CODE):
             for group in gro_m.get_campus_editor_groups(campus_code):
                 for gm in gro_m.gws.get_members(group.get_group_id()):
-                    if (not gm.is_uwnetid() and
-                            gm.name is not None and
-                            not gm.name.startswith("u_eventcal")):
-                        ret_list.append(group.name)
+                    if gm.is_group() and not gm.name.startswith("u_eventcal"):
+                        if group.get_group_id() not in ret_list:
+                            ret_list.append(group.get_group_id())
 
         sender = get_cronjob_sender()
         message = "\n".join(ret_list)
