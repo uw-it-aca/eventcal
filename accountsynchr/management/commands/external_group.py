e9177cb5fff292aa531e78341eb4d284fd95a9c7
Fang Lin <fanglin@uw.edu>
2021-02-18 13:26:43 -0800

    clean up

--- accountsynchr/management/commands/external_group.py
+++ accountsynchr/management/commands/external_group.py
@@ -21,13 +21,7 @@ class Command(BaseCommand):
             for group in gro_m.get_campus_editor_groups(campus_code):
                 for gm in gro_m.gws.get_members(group.get_group_id()):
                     if gm.is_group() and not gm.name.startswith("u_eventcal"):
-                        print(gm)
-                    """
-                    if (not gm.is_uwnetid() and
-                            gm.name is not None and
-                            not gm.name.startswith("u_eventcal")):
                         ret_list.append(group.get_group_id())
-                    """
 
         sender = get_cronjob_sender()
         message = "\n".join(ret_list)
