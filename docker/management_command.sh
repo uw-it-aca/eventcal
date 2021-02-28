896352c43db0366842549a899e1d001b58e09c9c
Fang Lin <fanglin@uw.edu>
2021-02-27 22:13:00 -0800

    clean up

--- docker/management_command.sh
+++ docker/management_command.sh
@@ -4,11 +4,9 @@ set -e
 if [ ! -e git-crypt-key ]; then
   echo "$GIT_CRYPT_KEY" > git-crypt-key.encoded
   base64 -d git-crypt-key.encoded > /app/git-crypt-key
-  cd /app/csv
   git-crypt unlock /app/git-crypt-key
 fi
 
-cd /app/
 source "/app/bin/activate"
 
 python3 manage.py $@
