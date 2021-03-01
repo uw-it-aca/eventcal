10706661ef60ee0159d539b6fbbf459de712c431
Fang Lin <fanglin@uw.edu>
2021-03-01 10:07:13 -0800

    turn off git-crypt unlock

--- docker/management_command.sh
+++ docker/management_command.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-if [ ! -e git-crypt-key ]; then
-  echo "$GIT_CRYPT_KEY" > git-crypt-key.encoded
-  base64 -d git-crypt-key.encoded > /app/git-crypt-key
-  git-crypt unlock /app/git-crypt-key
-fi
+#if [ ! -e git-crypt-key ]; then
+#  echo "$GIT_CRYPT_KEY" > git-crypt-key.encoded
+#  base64 -d git-crypt-key.encoded > /app/git-crypt-key
+#  git-crypt unlock /app/git-crypt-key
+#fi
 
 source "/app/bin/activate"
 
