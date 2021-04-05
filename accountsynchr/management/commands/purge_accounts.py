0eddb7a1bf8edc9bf020deb85815b254bebef5f9
Fang Lin <fanglin@uw.edu>
2021-04-05 10:42:36 -0700

    Add rerun of purge

--- accountsynchr/management/commands/purge_accounts.py
+++ accountsynchr/management/commands/purge_accounts.py
@@ -1,4 +1,5 @@
 import logging
+import time
 from django.core.mail import send_mail
 from django.core.management.base import BaseCommand, CommandError
 from accountsynchr.gws_trumba import GwsToTrumba
@@ -12,32 +13,41 @@ class Command(BaseCommand):
     """
     Purge inactive user accounts from Groups and Trumba.
     """
+    def run_purge(self):
+        msgs = []
+        purger = AccountPurger()
+        purger.set_accounts_to_purge()
+        msgs.append("Accounts closed in this run:")
+        for acc in purger.accounts_to_delete:
+            msgs.append("{},{}".format(acc.uwnetid, acc.last_visit))
+
+        purger.sync()
+        msgs.append("Total accounts purged: {}".format(
+            purger.total_accounts_deleted))
+        msgs.append("Total groups updated: {}".format(
+            purger.total_groups_purged))
+
+        if purger.has_err():
+            err = purger.get_error_report()
+            msgs.append(err)
+            logger.error(err)
+
+        message = "\n".join(msgs)
+        sender = get_cronjob_sender()
+        send_mail(
+            "Purge Inactive User Accounts",
+            message, sender, [sender])
 
     def handle(self, *args, **options):
         try:
-            msgs = []
-            purger = AccountPurger()
-            purger.set_accounts_to_purge()
-            msgs.append("Accounts closed in this run:")
-            for acc in purger.accounts_to_delete:
-                msgs.append("{},{}".format(acc.uwnetid, acc.last_visit))
-
-            purger.sync()
-            msgs.append("Total accounts purged: {}".format(
-                    purger.total_accounts_deleted))
-            msgs.append("Total groups updated: {}".format(
-                    purger.total_groups_purged))
-
-            if purger.has_err():
-                err = purger.get_error_report()
-                msgs.append(err)
-                logger.error(err)
-
-            message = "\n".join(msgs)
-            sender = get_cronjob_sender()
-            send_mail("Purge Inactive User Accounts",
-                message, sender, [sender])
+            self.run_purge()
         except Exception as ex:
             logger.error(ex)
-            send_mail("Purge Inactive User Account",
-                      "{}".format(ex), sender, [sender])
+            time.sleep(60)
+            try:
+                self.run_purge()
+            except Exception as ex1:
+                logger.error(ex1)
+                send_mail(
+                    "Purge Inactive User Account",
+                    "{}".format(ex1), sender, [sender])
