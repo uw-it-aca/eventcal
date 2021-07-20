0a845fbe55a03b6656a24bb094814616c6deb0e3
Jim Laney <jlaney@uw.edu>
2021-07-20 13:19:32 -0700

    add license/copyright to src files

--- accountsynchr/syncer.py
+++ accountsynchr/syncer.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from abc import ABCMeta, abstractmethod
 from accountsynchr.dao.trumba import CalPermManager
 from accountsynchr.ucalgroup.group_manager import GroupManager
