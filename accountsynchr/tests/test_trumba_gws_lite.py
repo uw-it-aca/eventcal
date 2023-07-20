b45c8d3a7d07a4721f6ea7ed2281a06f9f6fc66b
Fang Lin <fanglin@uw.edu>
2023-07-20 10:46:47 -0700

    inc test coverage

--- accountsynchr/tests/test_trumba_gws_lite.py
+++ accountsynchr/tests/test_trumba_gws_lite.py
@@ -3,7 +3,9 @@
 
 
 from django.test import TransactionTestCase
+from unittest.mock import patch
 from accountsynchr.models.gcalendar import GCalendar
+from accountsynchr.dao.gws import Gws
 from accountsynchr.trumba_gws_lite import TrumbaGwsLite
 from accountsynchr.tests import fdao_gws_override
 
@@ -11,11 +13,27 @@ from accountsynchr.tests import fdao_gws_override
 @fdao_gws_override
 class TestTrumbaGwsLite(TransactionTestCase):
 
-    def test_group_manager(self):
+    @patch.object(Gws, 'put_group')
+    def test_sync_success(self, mock):
+        mock.return_value = 1
         tg = TrumbaGwsLite()
         tg.sync()
-        self.assertEqual(tg.ttl_editor_grps_synced, 2)
+        self.assertEqual(tg.ttl_editor_grps_synced, 6)
+        self.assertEqual(tg.ttl_showon_grp_synced, 6)
+        self.assertEqual(tg.ttl_gcal_updated, 6)
+        self.assertFalse(tg.has_err())
+        self.assertTrue(len(tg.get_error_report()) == 0)
+        records = GCalendar.objects.all()
+        self.assertEqual(len(records), 6)
+
+    @patch.object(TrumbaGwsLite, 'put_editor_group')
+    def test_sync_showon_failure(self, mock):
+        mock.return_value = 1
+        tg = TrumbaGwsLite()
+        tg.sync()
+        self.assertEqual(tg.ttl_editor_grps_synced, 6)
         self.assertEqual(tg.ttl_showon_grp_synced, 2)
+        self.assertEqual(tg.ttl_gcal_updated, 2)
         self.assertTrue(tg.has_err())
         self.assertTrue(len(tg.get_error_report()) > 0)
         records = GCalendar.objects.all()
