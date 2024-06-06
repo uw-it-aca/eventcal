2b29307964cab99d3d487c9af419573107601ee7
Fang Lin <fanglin@uw.edu>
2024-06-06 12:36:54 -0700

    style fix

--- accountsynchr/migrations/0002_editorcreation.py
+++ accountsynchr/migrations/0002_editorcreation.py
@@ -13,8 +13,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='EditorCreation',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('uwnetid', models.CharField(db_index=True, max_length=128, unique=True)),
+                ('id', models.BigAutoField(
+                    auto_created=True, primary_key=True,
+                    serialize=False, verbose_name='ID')),
+                ('uwnetid', models.CharField(
+                    db_index=True, max_length=128, unique=True)),
                 ('last_updated', models.DateTimeField(auto_now=True)),
             ],
             options={
