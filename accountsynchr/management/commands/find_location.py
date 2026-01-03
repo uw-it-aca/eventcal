4ac3e53d93344453a565829346d37ed0ab2e2194
Fang Lin <fanglin@uw.edu>
2026-01-02 16:00:50 -0800

    Revise code

--- accountsynchr/management/commands/find_location.py
+++ accountsynchr/management/commands/find_location.py
@@ -2,8 +2,10 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import logging
+import re
 from django.core.management.base import BaseCommand
-from accountsynchr.dao.campus_location import get_campus_locations_from_spacews
+from accountsynchr.dao.campus_location import (
+  FAC, DataFailureException)
 
 logger = logging.getLogger(__name__)
 
@@ -15,9 +17,39 @@ class Command(BaseCommand):
             "name", type=str, help="Location name (code)")
 
     def handle(self, *args, **options):
-        self.name = options["name"]
-        campus_locations = get_campus_locations_from_spacews()
-        if campus_locations and len(campus_locations) > 0:
-            logger.info(f"Found {len(campus_locations)} records")
-            for bdg in campus_locations:
-                logger.info(f"{bdg.space_obj}\n")
+        name = options["name"]
+        res = re.match(r"^(.*)\(([^()]+)\)\s*$", name)
+        if not res:
+            logger.error(f"Could not parse {name}\n")
+            return
+        name = res.group(1).rstrip()
+        if res.group(2):
+            code = res.group(2).strip()
+        else:
+            code = ""
+        logger.info(f"name: {name}, code: {code}\n")
+        fac_objs = []
+        if code and len(code) > 1:
+            try:
+                fac_objs = FAC.search_by_code(code)
+                for fac in fac_objs:
+                    logger.info(f"{fac}\n")
+                return
+            except DataFailureException as ex:
+                logger.error(f"search_by_code {code} {ex}\n")
+        if name and len(name) > 1:
+            try:
+                fac_objs = FAC.search_by_name(name)
+                for fac in fac_objs:
+                    logger.info(f"{fac}\n")
+                return
+            except DataFailureException as ex:
+                logger.error(f"search_by_name {name} {ex}\n")
+
+            try:
+                fac_objs = FAC.search_by_street(name)
+                for fac in fac_objs:
+                    logger.info(f"{fac}\n")
+                return
+            except DataFailureException as ex:
+                logger.error(f"search_by_street {name} {ex}\n")
